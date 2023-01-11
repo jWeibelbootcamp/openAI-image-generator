fd1ac17e85a07f29fc3ab2609d720781e740cab2
devWeibel <weibel.jason@gmail.com>
2023-01-11 14:21:44 -0500

    tryin to fix heroku deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv').config();
-const port = process.env.PORT || 5000;
+const port = 443 || process.env.PORT ;
 
 const app = express();
 
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// static folder
+// static folder to use as root
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/openai', require('./routes/openaiRoutes'));

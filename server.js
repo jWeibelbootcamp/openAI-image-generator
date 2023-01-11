aaaa9a2504bbf5fd4fcc89c53c22e68b7af69c4a
devWeibel <weibel.jason@gmail.com>
2023-01-11 14:30:17 -0500

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv').config();
-const port = process.env.PORT || 443;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
